71c17e1c4a90d2226f9275c4c707a541e659c185
Bar011 <bar.cancer92@gmail.com>
2024-05-07 12:08:57 -0400

    hito 2: add models of Region and Comuna, add images to media files and data json

--- app/models.py
+++ app/models.py
@@ -18,6 +18,17 @@ class Usuario(User):
 
     def __str__(self):
         return f"{self.nombres} {self.apellidos}"
+    
+class Region(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, related_name='comunas', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
         
         
         
@@ -32,7 +43,7 @@ class Inmueble(models.Model):
     descripcion=models.CharField(max_length=50)
     imagen =models.ImageField(upload_to='')
     precio=models.DecimalField(max_digits=10, decimal_places=0)
-    comuna = models.CharField(max_length=50)
+    comuna = models.ForeignKey(Comuna,related_name ='inmuebles', on_delete =models.CASCADE, null = True)
     disponible=models.BooleanField(default=True)
     m2_construidos = models.DecimalField(max_digits=10, decimal_places=2)
     m2_terreno = models.DecimalField(max_digits=10, decimal_places=2)
